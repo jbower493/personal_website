6faa6d35165c09622a848ec9917be3ed7b4cadae
Jamie Bower <j_bower@hotmail.co.uk>
2024-09-29 13:13:31 +1000

    feat: finish contact and projects pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+import { Anchor } from "@/components/Anchor";
 import { PageTitle } from "@/components/PageTitle";
 import { Metadata } from "next";
 
@@ -10,6 +11,27 @@ export default function Contact() {
     return (
         <div>
             <PageTitle>Contact</PageTitle>
+            <div className="mb-8">
+                <h2 className="text-[24px] mb-2">LinkedIn</h2>
+                <Anchor
+                    type="external"
+                    href="https://www.linkedin.com/in/jamie-bower-a574581a2/"
+                >
+                    https://www.linkedin.com/in/jamie-bower-a574581a2/
+                </Anchor>
+            </div>
+            <div className="mb-8">
+                <h2 className="text-[24px] mb-2">Github</h2>
+                <Anchor type="external" href="https://github.com/jbower493">
+                    https://github.com/jbower493
+                </Anchor>
+            </div>
+            <div>
+                <h2 className="text-[24px] mb-2">Email</h2>
+                <Anchor type="external" href="mailto:jamiebower03@gmail.com">
+                    jamiebower03@gmail.com
+                </Anchor>
+            </div>
         </div>
     );
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -16,23 +16,28 @@ export default function Projects() {
                 <div className="w-full mb-5 flex gap-3 items-end">
                     <img
                         className="w-auto h-[280px] rounded-md"
-                        src="./projects/wp_desktop.jpg"
-                        alt="Workout Planner desktop view"
+                        src="./projects/sl_mob_1.png"
+                        alt="Shopping List first screenshot"
                     />
                     <img
-                        className="w-auto h-[240px] rounded-md"
-                        src="./projects/wp_mob.png"
-                        alt="Workout Planner mobile view"
+                        className="w-auto h-[280px] rounded-md"
+                        src="./projects/sl_mob_2.png"
+                        alt="Shopping List second screenshot"
+                    />
+                    <img
+                        className="w-auto h-[280px] rounded-md"
+                        src="./projects/sl_mob_3.png"
+                        alt="Shopping List third screenshot"
                     />
                 </div>
                 <div className="flex justify-between items-end mb-5">
                     <h3 className="text-[20px] text-S_text_primary leading-none">
-                        Workout Planner
+                        Shopping List
                     </h3>
                     <div className="flex gap-3">
                         <Button asChild variant="primary" size="sm">
                             <a
-                                href="https://jbwebsites.work/workout-planner"
+                                href="https://shoppinglist.jamiebowerdev.com"
                                 target="_blank"
                                 rel="noreferrer"
                             >
@@ -41,7 +46,7 @@ export default function Projects() {
                         </Button>
                         <Button asChild variant="outline" size="sm">
                             <a
-                                href="https://github.com/jbower493/portfolio-workout_planner"
+                                href="https://github.com/jbower493/shopping_list"
                                 target="_blank"
                                 rel="noreferrer"
                             >
@@ -51,12 +56,18 @@ export default function Projects() {
                     </div>
                 </div>
                 <p className="text-S_text_default">
-                    This is a web application which allows users to create a
-                    profile, then login and record their favourite workouts and
-                    exercises, including details about each exercise and
-                    workout. I used React for the frontend (with Redux for state
-                    management and Bootstrap for styling) and Node JS with
-                    Express for the backend API.
+                    This is an application I built to solve a problem that I was
+                    having in my own day to day life. I was fed up of writing
+                    shopping lists before doing the groceries each week,
+                    repeating the same items over and over, and figuring out
+                    what ingredients I needed to make certain recipes. This
+                    application simplifies and automates the process of writing
+                    your grocery list by allowing you to create recipes and
+                    menus, and then automatically add all the ingredients from
+                    those recipes into a list, from which you can then check off
+                    items as you walk around the store. It&apos;s available
+                    online and free to use, click the &quot;View Site&quot;
+                    button above to check it out!
                 </p>
             </div>
 
@@ -78,15 +89,6 @@ export default function Projects() {
                         Workout Planner
                     </h3>
                     <div className="flex gap-3">
-                        <Button asChild variant="primary" size="sm">
-                            <a
-                                href="https://jbwebsites.work/workout-planner"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                View Site
-                            </a>
-                        </Button>
                         <Button asChild variant="outline" size="sm">
                             <a
                                 href="https://github.com/jbower493/portfolio-workout_planner"
